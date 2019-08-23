c878eafc80e53b40ee6cab03cbcbe060b6de1680
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2019-08-23 13:21:06 +0300

    updated script

--- assets/essote-quick-service/form-script.js
+++ assets/essote-quick-service/form-script.js
@@ -12,5 +12,12 @@ $(document).on('metaformReady', function() {
   });
 
   var preferredContactDatePicker = document.querySelector("#field-prefered-contact-date")._flatpickr;
-  preferredContactDatePicker.set("disable", [new Date(2019, 0, 1), new Date(2019,0,6), new Date(2019,3,19), new Date(2019,3,21), new Date(2019,3,22), new Date(2019,3,30), new Date(2019,4,1), new Date(2019,4,30), new Date(2019,5,9), new Date(2019,5,21), new Date(2019,5,22), new Date(2019,10,2), new Date(2019,11,6), new Date(2019,11,24), new Date(2019,11,25), new Date(2019,11,26), new Date(2019,11,31) ]);
+  preferredContactDatePicker.set("disable", [
+        function(date) {
+            // return true to disable
+            return (date.getDay() === 0 || date.getDay() === 6);
+
+        },
+
+new Date(2019, 0, 1), new Date(2019,0,6), new Date(2019,3,19), new Date(2019,3,21), new Date(2019,3,22), new Date(2019,3,30), new Date(2019,4,1), new Date(2019,4,30), new Date(2019,5,9), new Date(2019,5,21), new Date(2019,5,22), new Date(2019,10,2), new Date(2019,11,6), new Date(2019,11,24), new Date(2019,11,25), new Date(2019,11,26), new Date(2019,11,31) ]);
 });
